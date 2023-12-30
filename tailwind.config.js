4e0e613d60c94e478e2f472a46e9a642db0c9155
Dishant Sahu <dishant.sahu07@gmail.com>
2023-12-30 16:04:22 +0530

    improve UI

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,17 +3,16 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {
-      colors : {
-        'bgclr-100x' : '#ef5a3c',
+      colors: {
+        "bgclr-100x": "#ef5a3c",
+        "bgclr-100x-nav": "#d14f36",
       },
-      fontFamily : {
-        'dm': ["DM Sans"] ,
-        'Inter' : ["Inter"]
-      }
-
-
+      fontFamily: {
+        dm: ["DM Sans"],
+        Inter: ["Inter"],
+        cursive: ["Dancing Script"],
+      },
     },
   },
   plugins: [],
-}
-
+};
